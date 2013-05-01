91f62f3ec8beda8b54841da0d1f9fd5451dacdd3
Graham Tremper <gtremper@gmail.com>
2013-04-30 23:21:43 -0700

    practicing with javascript

--- seam_carve.js
+++ seam_carve.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-	$("h1").replaceWith("<h1>HERP DERP</h1>");
+	$("h1")
+		.on("click", function(){
+			var element = $(this);
+			element.fadeOut(1000, function(){
+				if (element.text() === "HERP DERP") {
+					element.text("Seam Carving for CS283");
+				} else {
+					element.text("HERP DERP");
+				}
+				element.fadeIn(1000);
+			});
+		});
+		
+		
 });
