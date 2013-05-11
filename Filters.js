0b4524c685225a479e63f367f975b2e9fa8a8ced
Graham Tremper <gtremper@gmail.com>
2013-05-10 18:37:52 -0700

    removed unused convolute function in Filters. Working on adding extra members to paths so you can increase size of picture beyond original size

--- Filters.js
+++ Filters.js
@@ -1,48 +1,5 @@
 Filters = {};
 
-Filters.convolute = function(pixels, weights, opaque) {
-	var side = Math.round(Math.sqrt(weights.length));
-	var halfSide = Math.floor(side/2);
-	var src = pixels.data;
-	var sw = pixels.width;
-	var sh = pixels.height;
-	// pad output by the convolution matrix
-	var w = sw;
-	var h = sh;
-	var output = Filters.createImageData(w, h);
-	var dst = output.data;
-	// go through the destination image pixels
-	var alphaFac = opaque ? 1 : 0;
-	for (var y=0; y<h; y++) {
-		for (var x=0; x<w; x++) {
-			var sy = y;
-			var sx = x;
-			var dstOff = (y*w+x)*4;
-			// calculate the weighed sum of the source image pixels that
-			// fall under the convolution matrix
-			var r=0, g=0, b=0, a=0;
-			for (var cy=0; cy<side; cy++) {
-				for (var cx=0; cx<side; cx++) {
-					var scy = sy + cy - halfSide;
-					var scx = sx + cx - halfSide;
-					if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
-						var srcOff = (scy*sw+scx)*4;
-						var wt = weights[cy*side+cx];
-						r += src[srcOff] * wt;
-						g += src[srcOff+1] * wt;
-						b += src[srcOff+2] * wt;
-						a += src[srcOff+3] * wt;
-					}
-				}
-			}
-			dst[dstOff] = Math.abs(r);
-			dst[dstOff+1] = Math.abs(g);
-			dst[dstOff+2] = Math.abs(b);
-			dst[dstOff+3] = 255;
-		}
-	}
-	return output;
-};
 
 Filters.color_energy1 = function(pixels) {
 	var src = pixels.data;
@@ -139,6 +96,11 @@ function Pixel(index, r, g, b) {
 	};
 };
 
+Filters.add_extened_paths = function(paths){
+	return paths
+}
+
+
 Filters.get_paths = function(pixels) {
 	var w = pixels.width; // x
 	var h = pixels.height; // y
@@ -225,6 +187,9 @@ Filters.get_paths = function(pixels) {
 		list_of_paths.push(path);
 		w -= 1;
 	}
+	
+	list_of_paths = Filters.add_extened_paths(list_of_paths);
+	
 	return list_of_paths;
 };
 
@@ -273,8 +238,8 @@ Filters.get_path = function(pixels) {
   var paths = [];
   // compute the dynamic programming problem
   for (var y=1; y<h; y++) { // skip the first row
-    M[y*w] = 9999999999999999;
-    M[(y+1)*w-1] = 9999999999999999;
+    M[y*w] = Number.MAX_VALUE;
+    M[(y+1)*w-1] = Number.MAX_VALUE;
     for (var x=1; x<w-1; x++) {
       var offset = (y*w+x);
       var topleft = M[(y-1)*w+x-1];
@@ -296,7 +261,7 @@ Filters.get_path = function(pixels) {
   }
 
   // find index of the smallest value in the last row of M
-  var minvalue = 9999999999999999999999;
+  var minvalue = Number.MAX_VALUE;
   var index = -1;
   for (var i=M.length - w; i < M.length; i++) {
     if (M[i] < minvalue) {
@@ -324,8 +289,8 @@ Filters.get_horiz_path = function(pixels) {
     for (var i=0; i<h; i++) M[i*w] = 0;
     var paths = [];
     for (var x=1; x<w; x++) { // skip first column
-        M[x-1] = 99999999999999999999999999;
-        M[(x-1)+(h-1)*w] = 99999999999999999999999999999;
+        M[x-1] = Number.MAX_VALUE;
+        M[(x-1)+(h-1)*w] = Number.MAX_VALUE;
         for (var y=1; y<h-1; y++) {
             var lefttop = M[w*(y-1)+x-1];
             var leftmid = M[w*y+x-1];
@@ -350,7 +315,7 @@ Filters.get_horiz_path = function(pixels) {
     console.log(paths);
 
     // find index of smallest value in last column of M
-    var minvalue = 9999999999999999999999999;
+    var minvalue = Number.MAX_VALUE;
     var index = -1;
     for (var i=w-1; i< M.length; i+=w) {
         if (M[i] < minvalue) {
