4b4f0727fea172e9cdbf0991b17bd1d64c73af50
Gabe Fierro <gtfierro225@gmail.com>
2013-05-12 19:09:25 -0700

    try to get drop functionality for gradient canvas

--- seam_carve.js
+++ seam_carve.js
@@ -134,6 +134,22 @@ $(document).ready(function(){
 		evt.preventDefault();
 	}, false);
 
+    gradientcanvas.addEventListener('drop', function (e) {
+		var files = e.dataTransfer.files;
+		if (files.length > 0) {
+			var file = files[0];
+			if (typeof FileReader !== "undefined" && file.type.indexOf("image") != -1) {
+				var reader = new FileReader();
+				// Note: addEventListener doesn't work in Google Chrome for this event
+				reader.onload = function (e) {
+					gradientimg.src = e.target.result;
+				};
+				reader.readAsDataURL(file);
+			}
+		}
+		e.preventDefault();
+    }, false);
+
 	// Detect mousedown
 	canvas.addEventListener("mousedown", function (evt) {
 		mouseDown = true;
